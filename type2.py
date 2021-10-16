c76a06cadf7ff1178aa4e9475844045e3d7b6843
JangHyunJin <winckey3@gmail.com>
2021-10-16 17:11:20 +0000

    study 3

---
+++ type2.py
@@ -0,0 +1,24 @@
+
+
+t1 = (1, 2, 'a' , 'b')
+print(t1[3])
+print(t1.index(2))
+dic = {'name':'test' , 'age':1}
+
+
+print(dic)
+
+
+
+l = [1,2,2,3,4,5,6,6]
+l2 = [1,2,2,3,8,9,10,11]
+s1 = set(l)
+newlist = list(set(l))
+newlist2 = list(set(l2))
+print(newlist)
+print(newlist2)
+
+print(set(l).union(set(l2)))
+s1.union
+s1.intersection
+s1.
