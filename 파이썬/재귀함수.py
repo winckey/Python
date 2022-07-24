d06a97b4bf40b4d03fbbc85f877074e81eb5ff44
JangHyunJin <winckey3@gmail.com>
2022-07-24 10:37:24 +0000

    20220724

---
+++ 파이썬/재귀함수.py
@@ -0,0 +1,30 @@
+
+from cmath import pi
+import rlcompleter
+
+
+print((80+75+55)/3)
+
+a = 13
+if  (a %2 == 0 ) : 
+    print("짝수")
+else : 
+    print("홀수")
+    
+
+pin = "881120-1068234"
+
+
+if(pin[7] == '1' ) : 
+    print("남")
+else : 
+    print("여")
+    
+print(pin[7])
+
+
+
+rlist = [1, 3, 5, 4, 2] ;
+rlist.sort()
+rlist.reverse()
+print(rlist)
