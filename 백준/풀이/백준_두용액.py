e0b350d6575ffbb8088d88e444cacf317fd937c9
JangHyunJin <winckey3@gmail.com>
2022-08-14 00:23:53 +0900

    두 용액 다른풀이

--- 백준/풀이/백준_두용액.py
+++ 백준/풀이/백준_두용액.py
@@ -5,5 +5,35 @@ solution = list(map(int, input().split(' ')))
 # 정렬하기
 solution.sort()
 
-print(solution)
+start = 0
+end = n - 1 
+
+min = 9999999
+
+num1 =0;
+num2 =0;
+
+while start < end :
+    
+    nsum = solution[start] + solution[end]
+    if nsum == 0 : 
+        print(solution[start], solution[end])
+        exit(0)
+    
+    if abs(nsum) < min :
+        min = abs(nsum)
+        num1 = start
+        num2 = end
+    
+    
+    if  nsum < 0:
+        start += 1
+        
+
+    else :
+        end -= 1
+
+
+
+print(solution[num1], solution[num2])
 
