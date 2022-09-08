148f04cfde4be477b7523c06b92e5f043af12771
JangHyunJin <winckey3@gmail.com>
2022-09-08 10:53:31 +0000

    빙산 풀이 : 1 반복은deepcopy는 많은 시간을 요할수 있다 -> 자주 안일어 나게 사용 2 이차원 배열 모두 탐색보단 탐색해야하는것들을  좌표로 queue에 넣고 쓰기

---
+++ 백준/풀이/백준_빙산 2573 다른풀이.py
@@ -0,0 +1,60 @@
+import collections
+from copy import deepcopy
+import turtle
+
+n, m = map(int, input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
+dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
+
+
+def reduce (graph2) :
+
+    graph = deepcopy(graph2)
+    
+    for i in range(n) :
+        for j in range(m) :
+            if graph2[i][j] != 0 :
+                for tx , ty in zip(dx , dy) :
+                    if n > ty + i >=0 and m > tx + j >=0 and graph[ty + i][tx + j] == 0 :
+                        if graph2[i][j] == 0 :
+                            break;
+                        graph2[i][j] = graph2[i][j]-1;
+
+    return graph2
+    
+def bfs (graph) :
+    
+    visite = [[False] * m for _ in range(n) ]
+    
+    count = 0;
+    queue = collections.deque()
+    for i in range(n) :
+        for j in range(m) :
+            if graph[i][j] != 0 and not visite[i][j]:
+                queue.append((i,j))
+                visite[i][j] = True
+                while queue :
+                    y , x = queue.popleft()
+                    for tx , ty in zip(dx , dy) :
+                        ny = ty + y
+                        nx = tx + x
+                        if n > ny >=0 and m > nx >=0 and graph[ny][nx] != 0 and not visite[ny][nx]:
+                            queue.append( (ny , nx) )
+                            visite[ny][nx] = True
+                count = count +1
+    
+    if count == 2 :
+        return True
+    else : 
+        return False
+    
+year = 0;    
+    
+while True :
+    if bfs(graph) :
+        print(year)
+        break;
+    
+    graph = reduce(graph)
+    year = year+1
+        
