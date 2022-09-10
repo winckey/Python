8b80f02df51f6666953c86cc894f29856a61d725
JangHyunJin <winckey3@gmail.com>
2022-09-10 10:27:53 +0000

    백준 안전영역 : 그래프 및 변수 초기화 시점 항상 고민하기 반복시 이전 상태가 그래도 적용되면 찾기도 힘들다

--- 백준/풀이/백준_안전 영역 2468.py
+++ 백준/풀이/백준_안전 영역 2468.py
@@ -3,10 +3,16 @@
 from collections import deque
 import queue
 
-
+maxNum =0
 m = int(input())
-graph = [list(map(int, input().split())) for _ in range(m)]
-
+graph =[]
+for i in range(m):
+    graph.append(list(map(int, input().split())))
+    for j in range(m):
+        if graph[i][j] > maxNum:
+            maxNum = graph[i][j] 
+            
+            
 visited = [[False] * m for _ in range(m)]
 
 
@@ -20,23 +26,25 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 year = 0
 
+result =0
 
-count =0 
-for i in range(m):
-    for j in range(m):
-        
-        if graph[i][j] > m and not visited[i][j] :
-            visited[i][j] = True
-            queue.append((i,j))
-            while queue:
-                y , x = queue.popleft()
-                for ty , tx in zip(dy , dx) :
-                        ny = y+ ty
-                        nx = x+ tx
-                        if m > ny >=0 and m > nx >= 0 and not visited[ny][nx] and graph[ny][nx] > m:
-                            visited[ny][nx] = True
-                            queue.append((y,x))              
-            count = count +1                
+for h in range(maxNum):
+    count =0 
+    visited = [[False] * m for _ in range(m)]
+    for i in range(m):
+        for j in range(m):
+            if graph[i][j] > h and not visited[i][j] :
+                visited[i][j] = True
+                queue.append((i,j))
+                while queue:
+                    y , x = queue.popleft()
+                    for ty , tx in zip(dy , dx) :
+                            ny = y+ ty
+                            nx = x+ tx
+                            if m > ny >=0 and m > nx >= 0 and not visited[ny][nx] and graph[ny][nx] > h:
+                                visited[ny][nx] = True
+                                queue.append((ny,nx))              
+                count = count +1                
+    result = max(result  ,count)
     
-    
-print(count)
+print(result)
