5d20b4291ce7b9eb36eae474761098dfb0424f70
DESKTOP-PJ3UT06\JangHyunJin <winckey0@naver.com>
2022-11-24 10:29:32 +0900

    최대힙

---
+++ 백준/풀이/백준 최대힙.py
@@ -0,0 +1,12 @@
+import heapq
+
+heap = []
+values = [1,5,3,2,4]
+
+# 아래 for문을 실행시키고 나면 heap은 [-5,-4,-3,-1,-2]가 된다.
+for value in values:
+	heapq.heappush(heap, -value)
+
+# 아래 for문을 실행시키면 5,4,3,2,1이 출력된다. 즉, 큰 숫자부터 출력이 된다.
+for i in range(5):
+	print(-heapq.heappop(heap))
